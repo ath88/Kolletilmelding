48f55d826d785362f2271203fac5950338edf25e
Asbjoern Thegler <asbjoern@gmail.com>
2012-01-03 03:34:34 +0100

    error message works now, bad workaround though. needs to be improved

--- lib/Kolle/Controller.pm
+++ lib/Kolle/Controller.pm
@@ -16,35 +16,42 @@ sub frontpage {
 
   my $data = get_days();
 
-  return $self->render ( title => 'front', message => 'frontpage', dataset => $data );
+  my $error = $self->session->{'error'};
+  delete $self->session->{'error'};
+  return $self->render ( title => 'front', error => $error, dataset => $data );
 }
 
 sub day {
   my $self = shift;
   my $weekday = $self->stash('weekday');
+  if ( !day_exists($weekday) ) {
+    $log->warn("Day unknown '$weekday'");
+    $self->session->{'error'} = "Unknown weekday";
+    return $self->redirect_to ('/');
+  }
   $log->debug("Viewing day '$weekday'");
-
-  return $self->redirect_to ('/', error => "unknown weekday" ) if !day_exists($weekday);
 
   my $data => get_day($weekday);
 
-  return $self->render ( message => $weekday, dataset => $data );
+  return $self->render ( day => $weekday, dataset => $data );
 }
 
 sub postedit {
   my $self = shift;
   my $user_iden = $self->stash('id');
 
-  return $self->redirect_to ('/', error => "Don't do that, mkay?" ) if !user_exists($user_iden);
+  if ( !user_exists($user_iden) ) {
+    $self->session->{'error'} = "Don't do that, mkay?";
+    return $self->redirect_to ('/');
+  }
+
   $log->debug("Viewing postedit");
 
   my $postdata;
-  #update_user($user_iden);
+  #update_user($user_iden, $postdata);
   
 
   return edit ( $self, $postdata);
-
-  #$self->render ( message => 'postedit!');
 }
 
 sub edit {
@@ -53,11 +60,17 @@ sub edit {
   $log->debug("Viewing edit");
 
   # check if user exists, return error if not
-  return $self->redirect_to ('/', error => "$user_iden doesnt exist" ) if !user_exists($user_iden);
+  if ( !user_exists($user_iden) ) {
+    $self->session->{'error'} = "Unknown ID";
+    return $self->redirect_to ('/');
+  }
+
 
   my $data => get_user($user_iden);
 
-  return $self->render ( message => $user_iden, dataset => $data );
+  my $error = $self->session->{'error'};
+  delete $self->session->{'error'};
+  return $self->render ( message => $user_iden, error => $error, dataset => $data );
 }
 
 1;
