25b636c86dba25a91ed7c4a7f6ba1e96e166c838
Asbjørn Thegler <asbjoern@gmail.com>
2013-08-05 22:28:12 +0200

    new path

--- lib/Kolle.pm
+++ lib/Kolle.pm
@@ -11,7 +11,7 @@ sub startup {
   $self->plugin(tt_renderer => {
     template_options => {
       # These options are specific to TT
-      INCLUDE_PATH => "/home/ath88/work/Kolletilmelding/templates",
+      INCLUDE_PATH => "/home/ath88/repos/Kolletilmelding/templates",
       COMPILE_DIR => '/tmp/cache',
       COMPILE_EXT => '.ttc',
       # ... anything else to be passed on to TT should go here
--- script/run.sh
+++ script/run.sh
@@ -9,4 +9,4 @@ if [ $2 ]; then
   export MOJO_MODE=$2
 fi
 
-morbo /home/ath88/work/Kolletilmelding/script/kolle --verbose --listen http://0.0.0.0:$PORT
+morbo /home/ath88/repos/Kolletilmelding/script/kolle --verbose --listen http://0.0.0.0:$PORT
