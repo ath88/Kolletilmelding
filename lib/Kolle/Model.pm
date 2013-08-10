637e280b59002f8588627426c29ee39b9716a353
Asbjørn Thegler <asbjoern@gmail.com>
2013-08-10 11:11:38 +0200

    slight text change

--- lib/Kolle/Model.pm
+++ lib/Kolle/Model.pm
@@ -146,7 +146,7 @@ sub create_user {
   $name .= " $lastname" if $lastname;
 
   my $body = 
-"Hej $name,<br><br>Du er tilmeldt Mølleå Divisions Seniorkolleuge 2013. For at du kan deltage i bespisningen skal du fortælle hvornår du gerne vil spise med. Benyt derfor dette personlige link til at tilmelde dig, og rette din tilmelding.<br><br><a href='$baseurl$random_string'>Tilmelding</a><br><br>Hvis du ikke er $name, så svar venligst på mailen, så vi kan fejlfinde på problemet.<br><br>Med venlig hilsen<br>Mølleå Divisions Seniorkolleugeudvalg";
+"Hej $name,<br><br>Du er tilmeldt Mølleå Divisions Seniorkolleuge 2013. For at du kan deltage i bespisningen skal du fortælle hvornår du gerne vil spise med. Benyt derfor dette personlige link til at tilmelde dig, og efterfølgende rette din tilmelding.<br><br><a href='$baseurl$random_string'>Tilmelding</a><br><br>Hvis du ikke er $name, så svar venligst på mailen, så vi kan fejlfinde på problemet.<br><br>Med venlig hilsen<br>Mølleå Divisions Seniorkolleugeudvalg";
 
   my $mail = MIME::Entity->build(
     Type    => 'text/html',
